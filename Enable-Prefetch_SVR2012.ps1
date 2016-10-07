1f969ccb7f5fd83787686d6f09b4a883b2cb79db
WiredPulse <dj.nandy.nan@gmail.com>
2016-10-07 09:06:16 -0400

    Delete Enable-Prefetch_SVR2012.ps1

--- Enable-Prefetch_SVR2012.ps1
+++
@@ -1,9 +0,0 @@
-# Running this script will enable prefetch on a Server 2012 machine. No restart is needed.The script needs to be ran with elevated rights
-
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" /v EnablePrefetcher /t REG_DWORD /d 3 /f
- 
-reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Prefetcher" /v MaxPrefetchFiles /t REG_DWORD /d 8192 /f
- 
-Enable-MMAgent –OperationAPI
- 
-net start sysmain
