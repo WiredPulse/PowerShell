dca77fbe15b40089c0217a541ffd721f2647a3af
WiredPulse <dj.nandy.nan@gmail.com>
2016-10-07 09:07:48 -0400

    Delete Get-ADUser_Non-expiringAccounts.ps1

--- Get-ADUser_Non-expiringAccounts.ps1
+++
@@ -1,7 +0,0 @@
-<# Searches for non-expiring accounts
-#>
-
-
-import-module activedirectory
-
-Get-ADUser -Filter * -Properties passwordneverexpires | sort name | ft Name,passwordneverexpires,ObjectClass -A
