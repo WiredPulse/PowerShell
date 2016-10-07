3bd590572cbffde252525f2cf141381846043e5a
WiredPulse <dj.nandy.nan@gmail.com>
2016-10-07 09:09:09 -0400

    Delete Get-AdUser_PasswordChange.ps1

--- Get-AdUser_PasswordChange.ps1
+++
@@ -1,6 +0,0 @@
-<# Searches for accounts containing "svc" and displays the last time their password was changed.
-#>
-
-import-module activedirectory
-
-Get-ADUser -Filter 'name -Like "svc*"' | Get-ADUser -Properties PasswordLastSet | Select-Object -Property Name,PasswordLastSet | sort PasswordLastSet
