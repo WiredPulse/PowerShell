b16a8e0338e22f30808ca7fb8c1f76879787d492
WiredPulse <dj.nandy.nan@gmail.com>
2016-10-18 19:13:20 -0400

    Add files via upload

---
+++ Splunk/SplunkFWD_Remove_Initiator_v2.ps1
@@ -0,0 +1,28 @@
+﻿# Initiator for removing the Splunk Forwarder. This script initiates copies the SplunkFWD_Remover.ps1 script to a distant machine and runs it.
+
+# Reads in a list of computer names or IPs that you want the forwarder deleted from
+$computers = Get-Content .\computers.txt
+
+# The Splunk Forwarder to be deployed
+$muscle = "SplunkFWD_Remover.ps1"
+
+foreach($computer in $computers)
+{
+
+# Copies the SplunkForwarder_Remover_Muscle.ps1 script to the distant machine
+Copy-Item .\$muscle \\$computer\c$\.
+
+# Creates a variable for WMI process
+$Action = [wmiclass] "\\$computer\ROOT\CIMv2:Win32_Process"
+
+# Creates a process call to invoke the the uninstall Splunk muscle uninstall script
+$Method = $Action.create('powershell /c c:\SplunkFWD_Remover.ps1')
+
+# Allow time for the command to run
+sleep 10
+
+# Deletes the .msi after the uninstall is complete 
+remove-item \\$computer\c$\$muscle
+
+
+}
---
+++ Splunk/SplunkFWD_Remover.ps1
@@ -0,0 +1,55 @@
+﻿<# This script removes the Splunk Forwarder called "splunkforwarder-6.4.3-b03109c2bad4-x64-release.msi" from a group of supplied workstations. If you are not removing this 
+specific version, update line 52 with the applicable subject name.
+
+#>
+# Function to retrieve the extended file metadata
+Function Get-FileMetaData {
+
+[cmdletbinding()]
+param(
+    [Parameter(Mandatory = $true,
+               ValueFromPipeline = $true,
+               ValueFromPipelineByPropertyName = $true)]
+    [Alias('FullName', 'PSPath')]
+    [string[]]$path
+    )
+
+begin {
+    $oshell = New-Object -ComObject Shell.Application
+    }
+
+process {
+    $path | ForEach-Object {
+
+        if (test-path -path $_ -pathtype leaf) {
+            $fileitem = Get-Item -path $_
+
+            $ofolder = $oshell.namespace($fileitem.DirectoryName)
+            $oitem = $ofolder.Parsename($fileitem.Name)
+
+            $props = @{}
+
+            0..287 | ForEach-Object{
+                $EXTPropName = $ofolder.getdetailsof($ofolder.items, $_)
+                $EXTValName = $ofolder.GetDetailsof($oitem, $_)
+
+                if (-not $props.containskey($extpropname) -and
+                        ($EXTPropName -ne '')) {
+                            $props.add($extpropname, $extvalname)
+                            
+                }
+                }
+                New-object PSobject -property $props
+                }
+                }
+                }
+                end {
+                    $oshell = $null
+                    }
+                    }
+
+# Retrieves a listing of where all .msi are cached on the system and looks for the Splunk one. 
+$installer_name = gci C:\windows\Installer | Get-FileMetaData | Where-Object {$_.subject -eq "splunk UniversalForwarder wix 1.0 installer"} | select name -ExpandProperty name
+
+# Uninstalls the Splunk Forwarder
+msiexec.exe /uninstall c:\windows\installer\$installer_name.msi /quiet
