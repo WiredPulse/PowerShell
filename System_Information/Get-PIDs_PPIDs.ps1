908d98b4514bef13dd777351bfe926de9df70028
WiredPulse <dj.nandy.nan@gmail.com>
2016-10-21 07:23:43 -0400

    Delete Get-PIDs_PPIDs.ps1

--- System_Information/Get-PIDs_PPIDs.ps1
+++
@@ -1,3 +0,0 @@
-# Retrieves all PIDs and with their PPIDs
-
-get-wmiobject win32_process -computername localhost | select name, processid, parentprocessid, handle, handlecount, executablepath, creationdate | ft -autosize
