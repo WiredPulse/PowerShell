5c395057bd7ce90a540b05556dde3700beb72d0c
WiredPulse <dj.nandy.nan@gmail.com>
2016-11-15 23:55:13 -0500

    Delete Get-ADUser_PasswordChange2.ps1

--- Active_Directory/Get-ADUser_PasswordChange2.ps1
+++
@@ -1,7 +0,0 @@
-<# Lists all Domain Admins and displays the date the password was last changed.
-#>
-
-
-import-module activedirectory
-
-Get-ADGroupMember -Identity "Domain Admins" | Get-ADUser -Properties PasswordLastSet | Select-Object -Property Name,PasswordLastSet | sort PasswordLastSet 
