16767fba93bec023120abf60c128a87392a81b4d
WiredPulse <dj.nandy.nan@gmail.com>
2016-11-17 00:09:37 -0500

    Add files via upload

---
+++ Active_Directory/Set-MassPasswordChange2.ps1
@@ -0,0 +1,12 @@
+﻿# Sets a specific password to all users that are members of a specified group. 
+
+# NOTE: Be aware the a user can be a member of multiple groups.
+
+import-module activedirectory
+
+$some_group = "Group Name"
+$new_pass = "Input New Password to set"
+
+Get-ADGroupMember -Identity $some_group | Set-ADAccountPassword -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $new_pass -Force)
+
+Get-ADGroupMember -Identity $some_group | Set-aduser -changepasswordatlogon $true
