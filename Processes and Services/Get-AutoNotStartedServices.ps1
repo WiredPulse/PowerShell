cb1c08b620b247ef1d79c366420909450bca4bff
WiredPulse <WiredPulse@users.noreply.github.com>
2016-12-27 12:45:03 -0500

    Add files via upload

---
+++ Processes and Services/Get-AutoNotStartedServices.ps1
@@ -0,0 +1,4 @@
+﻿# Gets a table of services that are set to Automatic and are not started.
+
+$computers = Get-Content C:\users\blue\Desktop\computers.txt
+Get-wmiobject win32_service -ComputerName $computers -Filter "startmode = 'Auto' AND state != 'running' "| select PSComputername, name, pathname, startname | Export-CSV .\Get-AutoNotStartedServices.csv -NoTypeInformation
