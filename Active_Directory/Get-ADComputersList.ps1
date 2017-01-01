6bd443a0c5aa54974ed041f2d2bf650c99304ba3
WiredPulse <WiredPulse@users.noreply.github.com>
2017-01-01 03:05:12 -0500

    Add files via upload

---
+++ Active_Directory/Get-ADComputersList.ps1
@@ -0,0 +1,37 @@
+﻿    # Gets a perfectly formatted list of computer names from a domain and writes it to c:\
+    
+    $DN = ""
+    $DNSName = ""
+    $DNSArray = ""
+    $objSearcher = ""
+    $colProplist = ""
+    $objComputer = ""
+    $objResults = ""
+    $colResults = ""
+    $Computer = ""
+    $comp = ""
+    New-Item -type file -force "$Script:Folder_Path\Computer_List.txt" | Out-Null
+    $Script:Compute = "$Script:Folder_Path\Computer_List.txt"
+    $strCategory = "(ObjectCategory=Computer)"
+        $DNSName = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().Name
+        If($DNSName -ne $Null) {
+            $DNSArray = $DNSName.Split(".") 
+            for ($x = 0; $x -lt $DNSArray.Length ; $x++) {  
+                if ($x -eq ($DNSArray.Length - 1)){$Separator = ""}else{$Separator =","} 
+                [string]$DN += "DC=" + $DNSArray[$x] + $Separator  } }
+        $Script:Domain = $DN
+        Write-Host "Pulled computers from: "$Script:Domain -ForegroundColor Green
+        $objSearcher = New-Object System.DirectoryServices.DirectorySearcher("LDAP://$Script:Domain")
+        $objSearcher.Filter = $strCategory
+        $objSearcher.PageSize = 100000
+        $objSearcher.SearchScope = "SubTree"
+        $colProplist = "name"
+        foreach ($i in $colPropList) {
+            $objSearcher.propertiesToLoad.Add($i) }
+        $colResults = $objSearcher.FindAll()
+        foreach ($objResult in $colResults) {
+            $objComputer = $objResult.Properties
+            $comp = $objComputer.name
+            Write-output $comp | Out-File $Script:Compute -Append }
+        $Script:Computers = (Get-Content $Script:Compute) | Sort-Object
+        Write-Host "List of computers written to c:\computer_list.txt" -ForegroundColor Green
