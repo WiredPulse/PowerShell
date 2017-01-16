313174ea160d72281b128860485e2d95909a41ef
WiredPulse <WiredPulse@users.noreply.github.com>
2017-01-16 07:37:36 -0500

    Add files via upload

---
+++ System_Information/Get-DirListing2.ps1
@@ -0,0 +1,2 @@
+﻿# Recursively gets file listing and metadata
+gci c:\ -force | select Name, Fullname, Extension, Mode, Length, CreationTime, LastAccessTime, LastWriteTime, Attributes, IsReadOnly | export-csv dir_list.csv
