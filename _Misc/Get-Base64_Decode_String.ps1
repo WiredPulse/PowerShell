20f3d352dfa3cad6c016573965b2469caf89f14a
WiredPulse <WiredPulse@users.noreply.github.com>
2017-03-16 23:19:40 -0400

    Add files via upload

---
+++ _Misc/Get-Base64_Decode_String.ps1
@@ -0,0 +1,5 @@
+﻿# Takes encoded Base64 string and decodes it.
+
+Write-host -ForegroundColor cyan "Input Base64 string to decode"
+$base64string = read-host " " 
+[System.Text.Encoding]::UTF8.GetString(([System.Convert]::FromBase64String($base64string)|?{$_}))
---
+++ _Misc/Get-Base64_Encode_String.ps1
@@ -0,0 +1,7 @@
+﻿# Base64 encodes inputted data
+
+Write-host -ForegroundColor cyan "Input data to Base64 encode"
+$command = read-host " "
+$bytes = [system.text.encoding]::unicode.getbytes($command)
+$encodedCommand = [convert]::ToBase64String($bytes) 
+$encodedCommand
