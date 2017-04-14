6f313e1988cac3e2675859acc325ccc5efd6522d
WiredPulse <fernando.tomlinson@gmail.com>
2017-04-13 20:57:49 -0400

    .

---
+++ System_Information/Get-OSArchitecture.ps1
@@ -0,0 +1,9 @@
+﻿<#
+.SYNOPSIS
+    Retrieves the OS architecture or the supplied systems
+#>
+
+write-host 'Input the path to the list of systems to retrieve the OS architecture for.' -ForegroundColor Cyan
+$list = read-host " "
+
+Get-WmiObject Win32_OperatingSystem -computername $list | select PSComputerName, OSArchitecture
