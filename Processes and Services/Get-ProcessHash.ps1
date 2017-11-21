579f553b4a9d5748ca67f5d527e336d37280e4ca
WiredPulse <WiredPulse@users.noreply.github.com>
2017-11-21 07:43:03 -0500

    Add files via upload

---
+++ Processes and Services/Get-ProcessHash.ps1
@@ -0,0 +1,12 @@
+﻿foreach ($proc in get-process)
+    {
+    try
+        {
+        Get-FileHash $proc.path -Algorithm SHA1 -ErrorAction stop
+        }
+    catch
+        {
+         #error handling... log contains names of processes where there was no path listed or we lack the rights
+         $proc.name | out-file c:\proc_hash_error.log -Append
+        }
+    }
