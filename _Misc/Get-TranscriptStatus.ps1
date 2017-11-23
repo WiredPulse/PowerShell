7ddf7916543db8b8400df63c4b241607cf789149
WiredPulse <WiredPulse@users.noreply.github.com>
2017-11-23 00:27:00 -0500

    Add files via upload

---
+++ _Misc/Get-TranscriptStatus.ps1
@@ -0,0 +1,34 @@
+<#
+Returns whether the host is transcribing and if so, provides the file name and creation date
+#>
+
+﻿function Get-TranscriptStatus
+    {
+    try
+        {
+        $exHost = $host.gettype().getproperty("ExternalHost",[reflection.bindingflags]"nonpublic,instance").getvalue($host, @())
+        $scriptpath = $exhost.gettype().getfield("transcriptFileName", "nonpublic,instance").getvalue($externalhost)
+        }
+    catch
+        {
+        Write-Warning "The ISE doesn't support this feature."
+        }
+
+    $file = get-item $scriptpath
+
+    try
+        {
+	    $stream = New-Object system.IO.StreamReader $scriptpath
+	    if ($stream)
+            {
+            $stream.Close()
+            Write-Warning "Host is not transcribing"
+            }
+        }
+    catch
+        {
+        write-host "Host is transcribing..." -ForegroundColor green
+        write-host "Transcript file : " -ForegroundColor green -NoNewline; write-host $file -ForegroundColor yellow
+        write-host "Creation Time : " -ForegroundColor green -NoNewline; write-host $file.creationtime -ForegroundColor yellow
+        }
+    }
