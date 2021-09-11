2873e0f17d97ece15e6d62601d5ce55af6be4dc2
Fernando Tomlinson <WiredPulse@users.noreply.github.com>
2021-09-10 21:45:46 -0400

    Add files via upload

---
+++ Linux/ScriptBlock_Parser.ps1
@@ -0,0 +1,13 @@
+# Scriptblock
+$logs = get-content "/var/log/messages"
+$obj = foreach($log in $logs){
+    $logSplit = $log -Split '#012'
+    $time = ($logSplit[0] -Split ' CentOS')[0]
+
+    [PSCustomObject]@{
+        Time = $time
+        Command = $logSplit[1]
+    }
+}
+
+$obj
