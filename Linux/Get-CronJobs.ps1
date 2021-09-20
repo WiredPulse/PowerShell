00d76c773f76d7c3cc8c337f36786f1cd1515971
Fernando Tomlinson <WiredPulse@users.noreply.github.com>
2021-09-20 14:59:57 -0400

    Update Get-CronJobs.ps1

--- Linux/Get-CronJobs.ps1
+++ Linux/Get-CronJobs.ps1
@@ -1,18 +1,21 @@
+
 $users = get-content /etc/passwd
 $obj = @()
 $obj = foreach($user in $users){
     $user = $user -split ':'
     $task = crontab -u $user[0] -l 2>&1
     if($task -notlike "no*"){
-        $settings = $task[0..8]
-        [PSCustomObject]@{
-            User = $user[0]
-            Minute = $settings[0]
-            Hour = $settings[2]
-            DayOfMonth = $settings[4]
-            Month = $settings[6]
-            DayOfWeek = $settings[8]
-            Command = $task[10..$task.length[-1]] -join ''
+        foreach($userTask in $task){
+            $settings = $usertask -split ' '
+            [PSCustomObject]@{
+                User = $user[0]
+                Minute = $settings[0]
+                Hour = $settings[1]
+                DayOfMonth = $settings[2]
+                Month = $settings[3]
+                DayOfWeek = $settings[4]
+                Command = $settings[5..$settings.length[-1]] -join ' '
+            }
         }
     }
     else{
